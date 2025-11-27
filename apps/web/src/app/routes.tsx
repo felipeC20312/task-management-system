1e0a82ae2bd46a08841686c2c33d1c6f9da8b04f
felipeC20312 <felipecaetanodev@gmail.com>
2025-11-27 03:19:20 -0300

    Update - 271125.1
    Configuração do Tanstak Router, shadCnUi, TailwindCss, global.css e
    criação de features (auth/login, auth/register, dashboard/dashboard)

---
+++ apps/web/src/app/routes.tsx
@@ -0,0 +1,55 @@
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  Outlet,
+} from '@tanstack/react-router';
+import { lazy } from 'react';
+
+const rootRoute = createRootRoute({
+  component: () => <Outlet />,
+});
+
+const loginRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/login',
+  component: lazy(() =>
+    import("@/features/auth/pages/login-page").then((m) => ({
+      default: m.LoginPage
+    }))
+  ),
+});
+
+const registerRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/register',
+  component: lazy(() =>
+    import("@/features/auth/pages/register-page").then((m) => ({
+      default: m.RegisterPage
+    }))
+  ),
+});
+
+const dashboardRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/dashboard',
+  component: lazy(() =>
+    import("@/features/dashboard/pages/dashboard-page").then((m) => ({
+      default: m.DashboardPage
+    }))
+  ),
+});
+
+const routeTree = rootRoute.addChildren([
+  loginRoute,
+  registerRoute,
+  dashboardRoute,
+]);
+
+export const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
---
+++ apps/web/src/features/auth/pages/login-page.tsx
@@ -0,0 +1,7 @@
+export function LoginPage() {
+  return (
+    <main className="w-full h-dvh">
+      <h1>LoginPage</h1>
+    </main>
+  )
+}
